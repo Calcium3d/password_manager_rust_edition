e6a553999533b738289306fc7e327092c1a571d3
Blue Chalk <thebluechalk@gmail.com>
2021-03-13 16:27:53 +0530

    initialised

---
+++ target/rls/debug/deps/password_manager_rust-492e3b3bf9d21d2c.d
@@ -0,0 +1,5 @@
+/Users/oishikdas/projects/rust/password_manager_rust/target/rls/debug/deps/password_manager_rust-492e3b3bf9d21d2c.rmeta: src/main.rs
+
+/Users/oishikdas/projects/rust/password_manager_rust/target/rls/debug/deps/password_manager_rust-492e3b3bf9d21d2c.d: src/main.rs
+
+src/main.rs:
